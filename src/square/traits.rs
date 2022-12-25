df80a9512bd9b33ded8a92e8b4eae69bee05073a
Denny Wong <denwong47@hotmail.com>
2022-12-25 18:05:05 +0000

    Implemented par_iter to SquareShapedArray.

--- src/square/traits.rs
+++ src/square/traits.rs
@@ -1,3 +1,6 @@
+use std::sync::{Arc, Mutex};
+use std::fmt::Debug;
+
 use duplicate::duplicate_item;
 
 use ndarray::{
@@ -12,6 +15,7 @@ use ndarray::{
     s,
     Slice,
 };
+use rayon::prelude::*;
 use super::func;
 
 use super::super::generic::{
@@ -24,7 +28,7 @@ use super::super::primitives::{
 
 
 pub trait SquareShapedArray<D, A, T>: ArrayProxiedMethods<D, A>
-where   A: Clone+InitValue
+where   A: Clone + Debug + InitValue + Sync + Send
 {
     fn from_mapped_array2_fn<F>(
         arr: &T,
@@ -32,7 +36,7 @@ where   A: Clone+InitValue
         workers: usize,
         mirror: Option<bool>,
     ) -> Array2<A>
-    where   F: Fn(ArrayView1<'_, A>, ArrayView2<'_, A>)->Array1<A>;
+    where   F: Fn(ArrayView1<'_, A>, ArrayView2<'_, A>)->Array1<A> + Sync + Send;
 }
 
 #[duplicate_item(
@@ -43,7 +47,7 @@ where   A: Clone+InitValue
     [ ArrayViewMut2<'a, A> ]            [ 'a, A ];
 )]
 impl<__impl_generics__> SquareShapedArray<Ix2, A, __array_type__> for Array2<A>
-where   A: Clone+InitValue
+where   A: Clone + Debug + InitValue + Sync + Send
 {
     /// 2-dimensional array from applying a function between each pair of values in a 1-dimensional array.
     /// 
@@ -60,7 +64,7 @@ where   A: Clone+InitValue
         workers: usize,
         mirror: Option<bool>,
     ) -> Array2<A>
-    where   F: Fn(ArrayView1<'_, A>, ArrayView2<'_, A>)->Array1<A>
+    where   F: Fn(ArrayView1<'_, A>, ArrayView2<'_, A>)->Array1<A> + Sync + Send
     {
         let mirror = mirror.unwrap_or(true);
 
@@ -69,8 +73,10 @@ where   A: Clone+InitValue
 
         let mut result = Array2::<A>::from_elem(shape, A::init_value());
 
+        let result_ref = Arc::new(Mutex::new(result));
+
         func::trapizoid_slices_of_lower_half(len, workers)
-             .iter()
+             .par_iter()
              .for_each(
                 | range | {
                     let range_factory = || range.clone();
@@ -83,16 +89,26 @@ where   A: Clone+InitValue
                             let e = arr.slice_axis(Axis(0), e_ref);
                             
                             let to_ref = s![row, ..=row];
-                            let mut to_slice = result.slice_mut(to_ref);
 
                             let calculated = f(s, e);
 
-                            to_slice.assign(&calculated.view());
+                            {
+                                let result_ref_t = Arc::clone(&result_ref);
+
+                                let mut result = result_ref_t.lock().unwrap();
+                                let mut to_slice = result.slice_mut(to_ref);
+                                to_slice.assign(&calculated.view());
+                            }
                         }
                     );
                 }
              );
 
+        result = Arc::try_unwrap(result_ref)
+                     .unwrap()
+                     .into_inner()
+                     .unwrap();
+        
         if mirror {
             func::mirror_along_diagonal(&mut result);
         }
